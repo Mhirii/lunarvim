a6d7a23d5e2285c4074878783d1f6c23d42c0fe8
Mhiri <ahmedmhiri218@gmail.com>
2023-10-13 09:42:42 +0100

    1

---
+++ config.lua
@@ -0,0 +1,93 @@
+lvim.leader = " "
+lvim.colorscheme = "tokyonight" -- set to a custom theme
+lvim.builtin.time_based_themes = true -- set false to use your own configured theme
+lvim.transparent_window = true -- enable/disable transparency
+lvim.debug = true
+vim.lsp.set_log_level "error"
+lvim.log.level = "warn"
+require("user.neovim").config()
+lvim.lsp.code_lens_refresh = true
+lvim.lsp.installer.setup.automatic_installation = false
+
+
+-- Customization
+-- =========================================
+lvim.builtin.sell_your_soul_to_devil = { active = false, prada = false, openai = false } -- if you want microsoft to abuse your soul
+lvim.builtin.lastplace = { active = false } -- change to false if you are jumping to future
+lvim.builtin.tabnine = { active = false } -- change to false if you don't like tabnine
+lvim.builtin.persistence = { active = true } -- change to false if you don't want persistence
+lvim.builtin.presence = { active = false } -- change to true if you want discord presence
+lvim.builtin.orgmode = { active = false } -- change to true if you want orgmode.nvim
+lvim.builtin.dap.active = true -- change this to enable/disable debugging
+lvim.builtin.fancy_statusline = { active = true } -- enable/disable fancy statusline
+lvim.builtin.fancy_wild_menu = { active = true } -- enable/disable cmp-cmdline
+lvim.builtin.fancy_diff = { active = true } -- enable/disable fancier git diff
+lvim.builtin.lua_dev = { active = true } -- change this to enable/disable folke/lua_dev
+lvim.builtin.test_runner = { active = true, runner = "ultest" } -- change this to enable/disable ultest or neotest
+lvim.builtin.cheat = { active = true } -- enable/disable cheat.sh integration
+lvim.builtin.sql_integration = { active = false } -- use sql integration
+lvim.builtin.smooth_scroll = "" -- for smoth scrolling, can be "cinnamon", "neoscroll" or ""
+lvim.builtin.neoclip = { active = true, enable_persistent_history = false }
+lvim.builtin.nonumber_unfocus = false -- diffrentiate between focused and non focused windows
+lvim.builtin.custom_web_devicons = false -- install https://github.com/Nguyen-Hoang-Nam/mini-file-icons
+lvim.builtin.harpoon = { active = true } -- use the harpoon plugin
+lvim.builtin.remote_dev = { active = false } -- enable/disable remote development
+lvim.builtin.cursorline = { active = true } -- use a bit fancier cursorline
+lvim.builtin.motion_provider = "hop" -- change this to use different motion providers ( hop or leap or flash)
+lvim.builtin.hlslens = { active = true } -- enable/disable hlslens
+lvim.builtin.csv_support = false -- enable/disable csv support
+lvim.builtin.sidebar = { active = false } -- enable/disable sidebar
+lvim.builtin.task_runner = "" -- change this to use different task runners ( "async_tasks" or "overseer" or "")
+lvim.builtin.winbar_provider = "treesitter" -- can be "filename" or "treesitter" or "navic" or ""
+lvim.builtin.metals = {
+  active = false, -- enable/disable nvim-metals for scala development
+  fallbackScalaVersion = "3.2.0-RC3",
+  serverVersion = "1.0.1",
+  bloopVersion = "1.5.11",
+}
+lvim.builtin.collaborative_editing = { active = false } -- enable/disable collaborative editing
+lvim.builtin.file_browser = { active = false } -- enable/disable telescope file browser
+lvim.builtin.sniprun = { active = false } -- enable/disable sniprun
+lvim.builtin.tag_provider = "symbols-outline" -- change this to use different tag providers ( symbols-outline or vista )
+lvim.builtin.global_statusline = false -- set true to use global statusline
+lvim.builtin.dressing = { active = false } -- enable to override vim.ui.input and vim.ui.select with telescope
+lvim.builtin.refactoring = { active = false } -- enable to use refactoring.nvim code_actions
+lvim.builtin.tmux_lualine = false -- use vim-tpipeline to integrate lualine and tmux
+lvim.builtin.lsp_lines = false -- enable/disable lsp_lines to display lsp virtual text below instead of behind
+lvim.builtin.legendary = { active = false } -- enable/disable legendary plugin ( ctrl-p command )
+lvim.builtin.tree_provider = "nvimtree" -- can be "neo-tree" or "nvimtree" or ""
+lvim.builtin.lir.active = false
+lvim.builtin.breadcrumbs.active = false
+lvim.builtin.illuminate.active = false
+lvim.builtin.noice = { active = false } -- enables noice.nvim and inc-rename.nvim
+lvim.builtin.go_programming = { active = false } -- gopher.nvim + nvim-dap-go
+lvim.builtin.python_programming = { active = false } -- swenv.nvim + nvim-dap-python + requirements.txt.vim
+lvim.builtin.web_programming = { active = false } -- typescript.nvim + package-info.nvim
+lvim.builtin.rust_programming = { active = false } -- rust_tools.nvim + crates.nvim
+lvim.builtin.cpp_programming = { active = false } -- clangd_extensions.nvim + make-tools.nvim
+lvim.builtin.cmp.cmdline.enable = false
+lvim.builtin.borderless_cmp = false
+lvim.builtin.colored_args = false -- if true then sets up hlargs.nvim
+lvim.builtin.bigfile.active = true
+lvim.builtin.indentlines.mine = false
+-- WARN: mind plugin is deprecated ( use with caution )
+lvim.builtin.mind = { active = false, root_path = "~/.mind" } -- enable/disable mind.nvim
+
+
+
+
+
+if vim.g.neovide then
+    -- Put anything you want to happen only in Neovide here
+  vim.g.neovide_transparency = 0.7
+  vim.o.guifont = "JetbrainsMono Nerd Font:h12"
+  vim.g.neovide_theme = 'auto'
+  vim.g.neovide_cursor_animate_in_insert_mode = true
+  vim.g.neovide_cursor_vfx_mode = "pixiedust"
+  vim.g.neovide_scroll_animation_length = 0.3
+end
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
