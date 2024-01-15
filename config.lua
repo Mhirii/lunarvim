3a2e5fcf55e5fac31611aee038b485d8a99d4766
Mhirii <ahmedmhiri218@gmail.com>
2024-01-15 22:27:01 +0100

    fresh

--- config.lua
+++ config.lua
@@ -1,17 +1,14 @@
+---@diagnostic disable: undefined-global
 -- Customization
 -- =========================================
-vim.opt.relativenumber = true
-vim.opt.wrap = true
-vim.wo.fillchars = "eob: "
 lvim.leader = " "
 lvim.colorscheme = "tokyonight" -- set to a custom theme
 lvim.transparent_window = true
 lvim.debug = false
-vim.lsp.set_log_level("error")
 lvim.log.level = "warn"
-require("user.neovim").config()
 lvim.lsp.code_lens_refresh = true
 lvim.lsp.installer.setup.automatic_installation = true
+require("user.neovim").config()
 
 -- Additional Plugins
 -- =========================================
--- lua/user/keybinds.lua
+++ lua/user/keybinds.lua
@@ -1,3 +1,7 @@
 local M = {}
 
+M.config=function ()
+  
+end
+
 return M
