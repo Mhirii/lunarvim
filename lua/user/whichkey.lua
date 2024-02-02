ebd3762329de68c20192448659ba021577bd345b
Mhirii <ahmedmhiri218@gmail.com>
2024-02-02 06:20:48 +0100

    add binds for git

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -38,6 +38,33 @@ lvim.builtin.which_key.mappings["c"] = {
 	w = { "<cmd>Telescope diagnostics<cr>", "󱂬 Diagnostics" },
 }
 
+lvim.builtin.which_key.mappings["g"] = {
+	name = "Git",
+	g = { "<cmd>lua require 'lvim.core.terminal'.lazygit_toggle()<cr>", "Lazygit" },
+	j = { "<cmd>lua require 'gitsigns'.next_hunk({navigation_message = false})<cr>", "Next Hunk" },
+	k = { "<cmd>lua require 'gitsigns'.prev_hunk({navigation_message = false})<cr>", "Prev Hunk" },
+	l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
+	p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
+	r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
+	R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
+	s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
+	u = {
+		"<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
+		"Undo Stage Hunk",
+	},
+	o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
+	b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+	c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
+	C = {
+		"<cmd>Telescope git_bcommits<cr>",
+		"Checkout commit(for current file)",
+	},
+	d = {
+		"<cmd>Gitsigns diffthis HEAD<cr>",
+		"Git Diff",
+	},
+}
+
 lvim.builtin.which_key.mappings["p"] = {
 	name = "Preview",
 	l = { "<cmd>Lazy<cr>", "󰒲 Lazy" },
