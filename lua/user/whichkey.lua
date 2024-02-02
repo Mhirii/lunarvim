624f20b4777d93b041a5c0459937467fa1813d3e
Mhirii <ahmedmhiri218@gmail.com>
2024-02-02 06:33:25 +0100

    add binds for search

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -80,3 +80,23 @@ lvim.builtin.which_key.mappings["q"] = {
 	l = { [[<cmd>lua require("persistence").load({ last = true })<cr>]], "󰋚 Load Last Session" },
 	d = { [[<cmd>lua require("persistence").stop()<cr>]], "󱫫 Stop Saving Session" },
 }
+
+lvim.builtin.which_key.mappings["s"] = {
+	name = "Search",
+	b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+	c = { "<cmd>Telescope colorscheme<cr>", "󱥚 Colorscheme" },
+	f = { "<cmd>Telescope find_files<cr>", "󰮗 Find File" },
+	h = { "<cmd>Telescope help_tags<cr>", "󰋖 Find Help" },
+	H = { "<cmd>Telescope highlights<cr>", "󰸱 Find highlight groups" },
+	M = { "<cmd>Telescope man_pages<cr>", "󱔘 Man Pages" },
+	r = { "<cmd>Telescope oldfiles<cr>", "󱋢 Open Recent File" },
+	R = { "<cmd>Telescope registers<cr>", "󱘟 Registers" },
+	t = { "<cmd>Telescope live_grep<cr>", "󱩾 Text" },
+	k = { "<cmd>Telescope keymaps<cr>", "󰥻 Keymaps" },
+	C = { "<cmd>Telescope commands<cr>", "󱆃 Commands" },
+	l = { "<cmd>Telescope resume<cr>", "󰋚 Resume last search" },
+	p = {
+		"<cmd>lua require('telescope.builtin').colorscheme({enable_preview = true})<cr>",
+		"Colorscheme with Preview",
+	},
+}
